5047188976eaf179261e3ec26443b637ea6280af
Gamblegearbox <gamblegearbox@googlemail.com>
2014-10-24 16:23:22 +0200

    changed formatting

--- lab-01-ruby-exercise-master/lib/person.rb
+++ lab-01-ruby-exercise-master/lib/person.rb
@@ -3,12 +3,11 @@ class Person
   attr_accessor :name, :hobbies
 
   def initialize
-    @hobbies = "mey"
+
   end
 
   def hobbies= hobbies_string
-
-        @hobbies = hobbies_string.split(',')
-      end
+    @hobbies = hobbies_string.split(',')
+  end
 
 end
