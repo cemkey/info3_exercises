40ba99b77f705c4c8c9c961d2d404e9d5498efa9
Gamblegearbox <gamblegearbox@googlemail.com>
2014-10-24 16:21:09 +0200

    first test from person_hobbies_spec.rb works

--- lab-01-ruby-exercise-master/lib/person.rb
+++ lab-01-ruby-exercise-master/lib/person.rb
@@ -3,5 +3,12 @@ class Person
   attr_accessor :name, :hobbies
 
   def initialize
+    @hobbies = "mey"
   end
+
+  def hobbies= hobbies_string
+
+        @hobbies = hobbies_string.split(',')
+      end
+
 end
